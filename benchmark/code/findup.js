f93659088b6cc01bb950c3f813199b8a22aef2c3
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-19 01:26:02 -0500

    build readme

--- benchmark/code/findup.js
+++ benchmark/code/findup.js
@@ -4,8 +4,4 @@
  * [findup-sync](https://github.com/cowboy/node-findup-sync)
  */
 
-var findup = require('findup-sync');
-
-module.exports = function (patterns, options) {
-  return findup(patterns, options);
-};
+module.exports = require('findup-sync');
