3226df8479d18f9d14b47ba885c55d2685501b6f
jonschlinkert <jon.schlinkert@sellside.com>
2016-02-03 21:45:01 -0500

    update deps, utils

--- utils.js
+++ utils.js
@@ -1,23 +1,19 @@
 'use strict';
 
 var utils = require('lazy-cache')(require);
-
-/**
- * Temporarily re-assign `require` to trick browserify into
- * including lazy-cached deps
- */
-
 var fn = require;
 require = utils;
 
 /**
- * Lazily required module dependencies
+ * Lazily required module dependencies (globbing/fs stuff is a great
+ * use case for lazy-evaluation)
  */
 
 require('is-glob');
 require('normalize-path', 'normalize');
 require('resolve-dir', 'resolve');
 require('micromatch', 'mm');
+require = fn;
 
 /**
  * Expose `utils`
